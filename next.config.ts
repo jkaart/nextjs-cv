bd24fdea2d985a153f965d145db4f1fed3ff216f
Jani Kaartinen <jkaart@gmail.com>
2026-03-31 16:44:11 +0300

    fix: webpack import error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  webpack: (config: {
+    resolve: { alias: { canvas: boolean; encoding: boolean } }
+  }) => {
+    // Handle PDF renderer
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+
+    return config
+  },
+  transpilePackages: ["@react-pdf/renderer"]
 }
 
 const withMDX = createMDX({
