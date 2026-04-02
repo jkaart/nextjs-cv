102bc0c32bb362c9db5f68c4031824ba8ddf42b2
Jani Kaartinen <jkaart@gmail.com>
2026-04-02 15:14:38 +0300

    fix: typo

--- src/utils/sortWorkExperiences.ts
+++ src/utils/sortWorkExperiences.ts
@@ -3,6 +3,6 @@ import type { WorkExperience } from '@/data/data'
 export const compareWorkExperiences = (a: WorkExperience, b: WorkExperience) =>
   b.startDate.getTime() - a.startDate.getTime()
 
-export const sortWorkExperiences = (WorkExperiences: WorkExperience[]) => {
-  return [...WorkExperiences].sort(compareWorkExperiences)
+export const sortWorkExperiences = (workExperiences: WorkExperience[]) => {
+  return [...workExperiences].sort(compareWorkExperiences)
 }
