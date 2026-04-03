41a454d11344e4e69774bdec5c28e9521e8c988b
Jani Kaartinen <jkaart@gmail.com>
2026-04-03 21:56:16 +0300

    feat added Footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { data } from '@/data/data'
+
+const Footer = () => (
+  <div className='w-full py-5 mt-2 text-center bg-slate-300 dark:bg-neutral-800'>
+    <span>
+      &#169; 2026 {data.me.firstName} {data.me.lastName}
+    </span>
+  </div>
+)
+
+export default Footer
