fc7dc657bc827a1320b02d4948c8e3714660091c
Jani Kaartinen <jkaart@gmail.com>
2026-04-03 19:56:14 +0300

    refactor: PDFProjects component to use formatProjectDates and formatTechnologiesString functions

--- src/app/components/PDFProject.tsx
+++ src/app/components/PDFProject.tsx
@@ -1,17 +1,33 @@
 import { Text, View } from '@react-pdf/renderer'
+import { formatProjectDates } from '@/utils/formatProjectDates'
+import { formatTechnologiesString } from '@/utils/formatTechnologiesString'
 import type { ProjectMetadata } from '@/utils/projects'
+import { styles } from './PDFResume'
 
 interface PDFProjectProps {
   project: ProjectMetadata
 }
 
-const PDFProject = ({ project }: PDFProjectProps) => (
-  <View>
-    <Text style={{ fontSize: '14px', fontWeight: 'bold' }}>
-      {project.title}
-    </Text>
-    <Text style={{ fontSize: '12px' }}>{project.summary}</Text>
-  </View>
-)
+const PDFProject = ({ project }: PDFProjectProps) => {
+  const technologies = project.technologies ? project.technologies.sort() : []
+
+  const { startDate, endDate } = formatProjectDates(
+    project.startDate,
+    project.endDate
+  )
+
+  return (
+    <View style={{ marginBottom: '10px' }}>
+      <Text style={styles.h6}>{project.title}</Text>
+      <Text style={{ marginBottom: '5px' }}>{project.summary}</Text>
+      <Text style={{ fontWeight: 'bold', marginBottom: '5px' }}>
+        {`Käytetyt teknologiat: ${formatTechnologiesString(technologies)}`}
+      </Text>
+      <Text>
+        {startDate} - {endDate}
+      </Text>
+    </View>
+  )
+}
 
 export default PDFProject
