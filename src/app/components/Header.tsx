c1db8d3b84671c01f1c5d01dcb109f06a24f0b84
Jani Kaartinen <jkaart@gmail.com>
2026-04-03 22:03:40 +0300

    fix: font weight in header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from './NavBar'
 
 const Header = () => (
   <div className='fixed w-full border-b bg-slate-300 dark:bg-neutral-800 z-50'>
-    <h1 className='text-center text-3xl semi-bold'>Ansioluettelo/CV</h1>
+    <h1 className='text-center text-3xl font-semibold'>Ansioluettelo/CV</h1>
     <NavBar className='lg:m-1' />
   </div>
 )
