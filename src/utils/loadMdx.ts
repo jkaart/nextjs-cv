a7218292ae482e31588c49d6a6de8b85434d1abb
Jani Kaartinen <jkaart@gmail.com>
2026-04-21 14:09:23 +0300

    fix: build warning

--- src/utils/loadMdx.ts
+++ src/utils/loadMdx.ts
@@ -4,7 +4,7 @@ import { readFileSync } from 'node:fs'
 import path from 'node:path'
 
 export const loadMdx = async (filePath: string, fileName: string) => {
-  const joinedFilePath = path.join(process.cwd(), filePath, fileName)
+  const joinedFilePath = path.join(/*turbopackIgnore: true*/process.cwd(), filePath, fileName)
 
   return readFileSync(joinedFilePath, 'utf-8')
 }
