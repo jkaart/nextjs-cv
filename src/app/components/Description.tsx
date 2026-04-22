dabe7b46b7b75e597f9746e25929636580d1504a
Jani Kaartinen <jkaart@gmail.com>
2026-04-22 14:51:46 +0300

    refactor(Description): simplify component by removing dynamic import

--- src/app/components/Description.tsx
+++ src/app/components/Description.tsx
@@ -1,11 +1,7 @@
-'use server'
-
 import MdxLayout from '@components/MDXLayout'
+import DescriptionContent from '@data/mdx/me/description.mdx'
 
-const Description = async () => {
-  const { default: DescriptionContent } = await import(
-    '@data/mdx/me/description.mdx'
-  )
+const Description = () => {
   return (
     <MdxLayout className='prose dark:prose-invert'>
       <DescriptionContent />
