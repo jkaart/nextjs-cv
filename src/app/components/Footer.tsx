10775762ad00843672d5c31b1ac5d820083a1bed
Jani Kaartinen <jkaart@gmail.com>
2026-04-23 16:18:47 +0300

    feat(footer): content update date

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,20 @@
 import { data } from '@data/data'
+import { dateToString } from '@utils/dateToString'
+import { getLastContentUpdate } from '@utils/projects'
 
-const Footer = () => (
-  <footer className='w-full py-5 text-center bg-slate-300 dark:bg-neutral-800'>
-    <span>
-      &#169; 2026 {data.me.firstName} {data.me.lastName}
-    </span>
-  </footer>
-)
+const Footer = async () => {
+  const lastUpdated = await getLastContentUpdate()
+
+  return (
+    <footer className='w-full py-5 text-center flex flex-col bg-slate-300 dark:bg-neutral-800'>
+      <span>
+        &#169; 2026 {data.me.firstName} {data.me.lastName}
+      </span>
+      <span>
+        Sisältö päivitetty viimeksi: {dateToString(lastUpdated, 'date')}
+      </span>
+    </footer>
+  )
+}
 
 export default Footer
