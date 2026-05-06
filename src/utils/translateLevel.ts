55271c4310c820f672192fd2a6f81c758ea0b184
Jani Kaartinen <jkaart@gmail.com>
2026-05-06 14:24:59 +0300

    feat(translateLevel): add translateLevel function for level translations

---
+++ src/utils/translateLevel.ts
@@ -0,0 +1,18 @@
+import type { Level } from '@types'
+
+export const translateLevel = (level: Level) => {
+  switch (level) {
+    case 'veryPoor':
+      return 'erittäin huono'
+    case 'poor':
+      return 'huono'
+    case 'good':
+      return 'hyvä'
+    case 'veryGood':
+      return 'tosi hyvä'
+    case 'excellent':
+      return 'erinomainen'
+    default:
+     throw TypeError('invalid level')
+  }
+}
