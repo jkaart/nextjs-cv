4043c125f493effb0a2ad9a4b7b28a83f0c71c8c
Jani Kaartinen <jkaart@gmail.com>
2026-05-10 03:03:46 +0300

    fix(Header): header margins and font size

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,16 @@
 import NavBar from '@components/NavBar'
 import ThemeChange from './ThemeChange'
 
-const Header = async () => {
-  return (
-    <header className='sticky top-0 w-full border-b bg-slate-300 dark:bg-neutral-800 z-50'>
-      <h1 className='text-center text-3xl font-semibold'>Ansioluettelo/CV</h1>
-      <div className='lg:flex lg:justify-between lg:ms-1 lg:me-2'>
-        <NavBar className='lg:m-1' />
-        <ThemeChange className='max-lg:hidden' />
-      </div>
-    </header>
-  )
-}
+const Header = async () => (
+  <header className='sticky top-0 w-full border-b mb-1 bg-slate-300 dark:bg-neutral-800 z-50'>
+    <h1 className='text-center text-3xl lg:text-4xl font-semibold'>
+      Ansioluettelo/CV
+    </h1>
+    <div className='lg:flex lg:justify-between lg:ms-1 lg:me-2 mb-3'>
+      <NavBar className='lg:m-1' />
+      <ThemeChange className='max-lg:hidden' />
+    </div>
+  </header>
+)
 
 export default Header
