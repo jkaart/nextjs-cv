00298645c81177a68e8752bb30b0635487f383b1
Jani Kaartinen <jkaart@gmail.com>
2026-05-14 03:22:40 +0300

    ci: rename variables for clarity and debug script

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,9 +1,19 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
-const src = path.join(process.cwd(), 'src/data/assets')
-const dest = path.join(process.cwd(), 'public/assets')
+const source = path.join(process.cwd(), 'src/data/assets')
+const destination = path.join(process.cwd(), 'public/assets')
 
-fs.rmSync(dest, { recursive: true, force: true })
-fs.mkdirSync(dest, { recursive: true })
-fs.cpSync(src, dest, { recursive: true })
+console.log('Preparing content...')
+console.log('Source:', source)
+console.log('Destination:', destination)
+
+fs.rmSync(destination, { recursive: true, force: true })
+fs.mkdirSync(destination, { recursive: true })
+fs.cpSync(source, destination, { recursive: true })
+
+console.log('source content:')
+console.log(fs.readdirSync(source))
+
+console.log('Files copied:')
+console.log(fs.readdirSync(destination))
