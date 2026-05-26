3716de790289721ef25ed6fbeddba6338ebcc2aa
Jani Kaartinen <jkaart@gmail.com>
2026-05-26 22:39:44 +0300

    fix: correct test descriptions in validateDevIcon tests for clarity chore: remove unused comments from mock data in data.ts fix: reorder commands in prepare:dev script for consistency

--- __tests__/validateDevIcon.test.ts
+++ __tests__/validateDevIcon.test.ts
@@ -2,23 +2,23 @@ import { allDevIcons } from '@allDevIconTypes'
 import { validateDevIcon } from '@utils/validators'
 
 describe('validateDevIcon', () => {
-  it('return nothing if valid css iconName', () => {
+  it('returns nothing if valid css iconName', () => {
     expect(allDevIcons).toContain('css')
     expect(() => validateDevIcon('css')).not.toThrow()
   })
 
-  it('return nothing if valid react iconName', () => {
+  it('returns nothing if valid react iconName', () => {
     expect(allDevIcons).toContain('react')
     expect(() => validateDevIcon('react')).not.toThrow()
   })
 
-  it('return Error if iconName not acceptable', () => {
+  it('returns Error if iconName not acceptable', () => {
     expect(() => validateDevIcon('random-text')).toThrow(
       'random-text is not a valid icon name'
     )
   })
 
-  it('return Error if iconName is empty string', () => {
+  it('returns Error if iconName is empty string', () => {
     expect(() => validateDevIcon('')).toThrow(
       'iconName must be a non-empty string'
     )
